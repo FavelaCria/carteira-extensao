58d3082eeaf6f5c631a6babcbbb5e7c1ec771cd3
georgewrmarshall <george.marshall@consensys.net>
2021-11-23 15:17:29 -0800

    Using IconWithFallback instead of SiteIcon, fixing icon prop, and adding status story and docs page

--- ui/components/ui/icon-with-fallback/icon-with-fallback.component.js
+++ ui/components/ui/icon-with-fallback/icon-with-fallback.component.js
@@ -30,7 +30,7 @@ export default class IconWithFallback extends PureComponent {
         src={icon}
         style={style}
         className={className}
-        alt=""
+        alt={name}
       />
     ) : (
       <i
